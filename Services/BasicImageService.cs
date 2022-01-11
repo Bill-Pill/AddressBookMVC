07dfc255da031d3f7558a7884ae4f5bdb659629d
Bill-Pill <delashmuttwa@gmail.com>
2022-01-10 19:23:45 -0500

    Register Image Service

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,24 @@
+﻿using AddressBookMVC.Services.Interfaces;
+
+namespace AddressBookMVC.Services
+{
+    public class BasicImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            if (fileData is null) return string.Empty;
+
+            string imageBase64data = Convert.ToBase64String(fileData);
+            return $"data:{extension};base64,{imageBase64data}";
+        }
+
+        public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            using MemoryStream memoryStream = new();
+            await file.CopyToAsync(memoryStream);
+            byte[] byteFile = memoryStream.ToArray();
+
+            return byteFile;
+        }
+    }
+}
